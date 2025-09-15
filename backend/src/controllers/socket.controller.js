ea3601fba901aa8a3228c61196bfb7bed91b84b1
Aqib Ansari <aqibansari72a@gmail.com>
2025-09-16 03:19:40 +0530

    refactor: rename socket events for clarity and consistency

--- backend/src/controllers/socket.controller.js
+++ backend/src/controllers/socket.controller.js
@@ -4,19 +4,46 @@ export default function socketController(io) {
   io.on("connection", (socket) => {
     socket.emit("load-board", boardStates);
 
-    socket.on("start", (data) => {
+    socket.on("start-line", (data) => {
       const newLine = { tool: data.tool, points: data.points };
       boardStates.push(newLine);
       io.emit("start", data);
     });
 
-    socket.on("drawing", (data) => {
+    socket.on("start-rect", (data) => {
+      const newRect = {
+        type: data.tool,
+        x: data.x,
+        y: data.y,
+        width: data.width,
+        height: data.height,
+        complete: false,
+      };
+      boardStates.push(newRect);
+      console.log("Rect start", boardStates);
+      io.emit("start-rect", data);
+    });
+
+    socket.on("drawing-line", (data) => {
       const lastLine = boardStates[boardStates.length - 1];
       if (lastLine) {
         lastLine.points.push(data.x, data.y);
       }
+      io.emit("drawing-line", data);
+    });
 
-      io.emit("drawing", data);
+    socket.on("drawing-rect", (data) => {
+      const lastRect = boardStates.findLast(
+        (item) => item.type === "rectangle" && !item.complete
+      );
+      console.log("Rect drawing", lastRect, data);
+      if (lastRect) {
+        lastRect.x = data.x;
+        lastRect.y = data.y;
+        lastRect.width = data.width;
+        lastRect.height = data.height;
+      }
+      io.emit("drawing-rect", data);
     });
 
     socket.on("end", () => {
@@ -24,7 +51,6 @@ export default function socketController(io) {
       if (lastLine) {
         lastLine.complete = true;
       }
-
       io.emit("end");
     });
 
