0da1e63e027162f0de659cf21e96dc35b6baf493
Aqib Ansari <aqibansari72a@gmail.com>
2025-09-17 02:20:26 +0530

    feat: add ellipse drawing functionality with socket event handling

--- frontend/src/lib/mouseEvents.ts
+++ frontend/src/lib/mouseEvents.ts
@@ -103,3 +103,63 @@ export const rectMouseMove = (
     });
   }
 };
+
+export const ellipseMouseDown = (
+  e: any,
+  tool: string,
+  setShapes: any,
+  isDrawingRef: any,
+  startPosRef: any,
+  socketRef?: React.MutableRefObject<any>
+) => {
+  isDrawingRef.current = true;
+  const point = getPointerPosition(e);
+  if (!point) return;
+  startPosRef.current = point;
+  setShapes((prev: any) => {
+    return [
+      ...prev,
+      { type: tool, x: point.x, y: point.y, radiusX: 0, radiusY: 0 },
+    ];
+  });
+  if (socketRef?.current) {
+    socketRef.current?.emit("start-ellipse", {
+      tool: tool,
+      x: point.x,
+      y: point.y,
+      radiusX: 0,
+      radiusY: 0,
+    });
+  }
+};
+
+export const ellipseMouseMove = (
+  e: any,
+  shapes: any,
+  setShapes: any,
+  isDrawingRef: any,
+  startPosRef: any,
+  socketRef?: React.MutableRefObject<any>
+) => {
+  if (!isDrawingRef.current) return;
+  const point = getPointerPosition(e);
+  if (!point) return;
+  const startPosX = startPosRef.current.x;
+  const startPosY = startPosRef.current.y;
+  const newRadiusX = Math.abs(point.x - startPosX);
+  const newRadiusY = Math.abs(point.y - startPosY);
+  let lastShape = shapes[shapes.length - 1];
+  if (!lastShape) return;
+  lastShape.radiusX = newRadiusX;
+  lastShape.radiusY = newRadiusY;
+  shapes.splice(shapes.length - 1, 1, lastShape);
+  setShapes(shapes.concat());
+  if (socketRef?.current) {
+    socketRef.current?.emit("drawing-ellipse", {
+      x: startPosX,
+      y: startPosY,
+      radiusX: newRadiusX,
+      radiusY: newRadiusY,
+    });
+  }
+};
